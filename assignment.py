3a165a57e13f7a87d71930a828f4ecbddca51f66
arpan28 <arpangaur007@gmail.com>
2025-03-03 14:40:34 -0500

    test empty string

--- assignment.py
+++ assignment.py
@@ -2,11 +2,18 @@ import unittest
 
 
 class StringCalculator:
-    pass
+    def Add(self, numbers: str) -> int:
+        if not numbers:
+            return 0
+
 
 class TestStringInput(unittest.TestCase):
     def setUp(self):
         self.calc = StringCalculator()
+
+    def test_empty_string(self):
+         self.assertEqual(self.calc.Add(""), 0)
+    
 
 if _name_ == "_main_":
     unittest.main()
