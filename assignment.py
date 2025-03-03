b034845db48f5b2abf22c144d2d445f4b972a53d
arpan28 <arpangaur007@gmail.com>
2025-03-03 14:36:33 -0500

    Initial commit

---
+++ assignment.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class StringCalculator:
+    pass
+
+class TestStringInput(unittest.TestCase):
+    def setUp(self):
+        self.calc = StringCalculator()
+
+if _name_ == "_main_":
+    unittest.main()
