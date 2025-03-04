7bc6d19bc7b85793655d315e882f796c2815dbd9
arpan28 <arpangaur007@gmail.com>
2025-03-03 21:07:39 -0500

    Added code to handle negative numbers

--- assignment.py
+++ assignment.py
@@ -9,6 +9,9 @@ class StringCalculator:
         delimiter = r"[,\n]"
         numbers = re.split(delimiter, numbers)
         nums = list(map(int, numbers))
+        negatives = [n for n in nums if n < 0]
+        if negatives:
+            raise ValueError(f"negatives not allowed {negatives}")
         return sum(n for n in nums if n <= 1000)
 
 class TestStringInput(unittest.TestCase):
@@ -31,6 +34,14 @@ class TestStringInput(unittest.TestCase):
 
     def test_ignore_numbers_greater_than_1000(self):
         self.assertEqual(self.calc.Add("2,1001"), 2)
+
+    def test_negative_numbers(self):
+        with self.assertRaises(ValueError) as context:
+            self.calc.Add("1,-2,3,-4")
+        self.assertEqual(str(context.exception), "negatives not allowed [-2, -4]")
+    
+   
+
 
 
     
